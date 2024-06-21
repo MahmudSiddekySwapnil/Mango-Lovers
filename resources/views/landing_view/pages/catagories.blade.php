657f6abf7cde311ce915be5e82cfc068e744a47b
MahmudSiddekySwapnil <mahmud.siddeky@shurjomukhisolutions.com.bd>
2024-06-21 21:13:09 +0600

    video added

--- resources/views/landing_view/pages/catagories.blade.php
+++ resources/views/landing_view/pages/catagories.blade.php
@@ -1,3 +1,25 @@
+<style>
+    .video-container {
+        position: relative;
+        padding-bottom: 56.25%; /* 16:9 ratio */
+        height: 0;
+        overflow: hidden;
+        max-width: 100%;
+        background: #000;
+        margin: 0 auto; /* Center the container */
+        border: 2px solid #ccc; /* Border */
+        border-radius: 10px; /* Rounded corners */
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Shadow */
+    }
+
+    .video-container iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+    }
+</style>
 <section class="section category-part">
     <div class="container">
        <div class="row">
@@ -8,7 +30,13 @@
           </div>
        </div>
        <div class="row">
-          <div class="col-lg-12">
+
+           <div class="col-lg-12">
+               <div class="video-container">
+                   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yCgJiYyr4lI" allowfullscreen></iframe>
+               </div>
+          &nbsp;&nbsp;
+
              <div class="category-slider slider-arrow">
                 <div class="category-wrap">
                    <div class="category-media">
@@ -52,7 +80,7 @@
                 </div>
                 <div class="category-wrap">
                    <div class="category-media">
-                      <img src="front_assets/images/mango/I.jpg" alt="category">
+                      <img src="front_assets/images/mango/client.jpg" alt="category">
                       <div class="category-overlay"><a href="shop-4column.html"><i class="fas fa-link"></i></a></div>
                    </div>
                    <div class="category-meta">
@@ -70,6 +98,29 @@
                        <p>Mangoes are tropical fruits known for their sweet, juicy flesh and vibrant orange color.</p>
                    </div>
                 </div>
+
+                 <div class="category-wrap">
+                     <div class="category-media">
+                         <img src="front_assets/images/mango/hasan.jpg" alt="category">
+                         <div class="category-overlay"><a href="shop-4column.html"><i class="fas fa-link"></i></a></div>
+                     </div>
+                     <div class="category-meta">
+                         <h4>Mango</h4>
+                         <p>Mangoes are tropical fruits known for their sweet, juicy flesh and vibrant orange color.</p>
+                     </div>
+                 </div>
+
+                 <div class="category-wrap">
+                     <div class="category-media">
+                         <img src="front_assets/images/mango/bo.jpg" alt="category">
+                         <div class="category-overlay"><a href="shop-4column.html"><i class="fas fa-link"></i></a></div>
+                     </div>
+                     <div class="category-meta">
+                         <h4>Mango</h4>
+                         <p>Mangoes are tropical fruits known for their sweet, juicy flesh and vibrant orange color.</p>
+                     </div>
+                 </div>
+
              </div>
           </div>
        </div>
