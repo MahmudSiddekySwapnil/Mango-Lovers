43994802ddb54df952345a76cc6b793746a677bf
MahmudSiddekySwapnil <mahmud.siddeky@shurjomukhisolutions.com.bd>
2024-06-25 16:08:12 +0600

    update error

--- resources/views/landing_view/pages/new_items.blade.php
+++ resources/views/landing_view/pages/new_items.blade.php
@@ -82,7 +82,7 @@
 <div class="mobile-menu">
     <a href="index.html" title="Home Page"><i class="fas fa-home"></i><span>Home</span></a>
     <button class="cate-btn" title="Category List"><i class="fas fa-list"></i><span>category</span></button>
-    <button class="cart-btn" title="Cartlist"><i class="fas fa-shopping-basket"></i><span>cartlist</span><sup class="cart-count" id="cart-ccount">0</sup></button>
+    <button class="cart-btn" title="Cartlist"><i class="fas fa-shopping-basket"></i><span>cartlist</span><sup class="cart-count" id="mobile-cart-count">0</sup></button>
     <a href="wishlist.html" title="Wishlist"><i class="fas fa-heart"></i><span>wishlist</span><sup>0</sup></a>
     <a href="compare.html" title="Compare List"><i class="fas fa-random"></i><span>compare</span><sup>0</sup></a>
 </div>
@@ -283,10 +283,13 @@
     });
 
     function getProductsFromLocalStorage() {
-        const cartCountElement = document.getElementById('cart-ccount');
+        const cartCountElementMobile = document.getElementById('mobile-cart-count');
+        const cartCountElementPC= document.getElementById('pc-cart-count');
+
         let products = JSON.parse(localStorage.getItem('products')) || [];
         let totalItems = products.length;
-        cartCountElement.textContent = totalItems.toString();
+        cartCountElementMobile.textContent = totalItems.toString();
+        cartCountElementPC.textContent = totalItems.toString();
         return products;
     }
 </script>
